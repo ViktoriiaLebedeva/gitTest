015074510866c242302dec190907e52e76cc6dce
Viktoriia Lebedeva <Viktoriia_Lebedeva@epam.com>
2019-11-29 12:56:02 +0300

    Added class person

---
+++ module2/src/main/java/com/epam/gitLearn/Person.java
@@ -0,0 +1,12 @@
+package com.epam.gitLearn;
+
+public class Person {
+    private  String name;
+    private byte age;
+
+    public Person(String name, byte age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
