6b32247f866b5637e5e5aa4731f34ccee4b0182f
Viktoriia Lebedeva <Viktoriia_Lebedeva@epam.com>
2019-11-29 12:52:32 +0300

    Added new module

---
+++ module2/src/main/java/com/epam/gitLearn/Class2.java
@@ -0,0 +1,13 @@
+package com.epam.gitLearn;
+
+/**
+ * Hello world!
+ *
+ */
+public class Class2
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ module2/src/test/java/com/epam/gitLearn/Class2Test.java
@@ -0,0 +1,20 @@
+package com.epam.gitLearn;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class Class2Test
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
