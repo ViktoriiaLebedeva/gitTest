1f4accb4a565078a01766e1b2141699bbd9f4f7e
Viktoriia Lebedeva <Viktoriia_Lebedeva@epam.com>
2019-11-29 13:49:52 +0300

    Added class animal and field animal to person

---
+++ module2/src/main/java/com/epam/gitLearn/HomeAnimal.java
@@ -0,0 +1,17 @@
+package com.epam.gitLearn;
+
+public class HomeAnimal {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public HomeAnimal(String name) {
+        this.name = name;
+    }
+}
--- module2/src/main/java/com/epam/gitLearn/Person.java
+++ module2/src/main/java/com/epam/gitLearn/Person.java
@@ -3,10 +3,14 @@ package com.epam.gitLearn;
 public class Person {
     private  String name;
     private byte age;
+    private HomeAnimal animal;
 
     public Person(String name, byte age) {
         this.name = name;
         this.age = age;
     }
 
+    public void setAnimal(HomeAnimal animal) {
+        this.animal = animal;
+    }
 }
